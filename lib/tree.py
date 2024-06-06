0e3747b357a2d169423ff184069730613171261c
justin <justinmwathi32@gmail.com>
2024-06-06 12:08:17 +0300

    Lab completed

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,31 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
+  
   def get_element_by_id(self, id):
-    pass
+
+        if self.root is None:
+
+            return None
+
+        return self._get_element_by_id(self.root, id)
+
+
+  def _get_element_by_id(self, node, id):
+
+        if node.get('id') == id:
+
+            return node
+
+        for child in node.get('children', []):
+
+            element = self._get_element_by_id(child, id)
+
+            if element is not None:
+
+                return element
+
+        return None
+
+
+
